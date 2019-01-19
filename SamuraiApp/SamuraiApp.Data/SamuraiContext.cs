ef5e96deba32cc65acf77b41273362582123d52a
vatansoni <vatansoni89@gmail.com>
2019-01-20 00:58:15 +0530

    1. Different data types can be added at once. 2. Even we can add directly to context. 2. Batchsize can be configured in OnConfiguring()

--- SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -3,16 +3,24 @@ using SamuraiApp.Domain;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 
 namespace SamuraiApp.Data
 {
    public class SamuraiContext : DbContext
     {
-        public SamuraiContext(DbContextOptions<SamuraiContext> options) :base(options)
-        {
+        //public SamuraiContext(DbContextOptions<SamuraiContext> options) :base(options)
+        //{
 
-        }
+        //}
+
+        public static readonly LoggerFactory MyConsoleLoggerFactory
+            = new LoggerFactory(new[] {
+                new ConsoleLoggerProvider((category, level) 
+                    => category ==DbLoggerCategory.Database.Command.Name
+                && level == LogLevel.Information, true)});
 
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
@@ -20,9 +28,11 @@ namespace SamuraiApp.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseSqlServer(
-            //     "Server = (localdb)\\mssqllocaldb; Database = SamuraiAppData; Trusted_Connection = True; ");
-
+            optionsBuilder
+                .UseLoggerFactory(MyConsoleLoggerFactory)
+                .UseSqlServer(
+                 "Server=(localdb)\\mssqllocaldb;Database=SamuraiAppDataCore;Trusted_Connection=True;MultipleActiveResultSets=true")
+                 .EnableSensitiveDataLogging();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- SamuraiApp/SomeUI/Program.cs
+++ SamuraiApp/SomeUI/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using SamuraiApp.Domain;
+using SamuraiApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace SomeUI
 {
@@ -6,7 +9,54 @@ namespace SomeUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //InsertSamurai();
+            //InsertMultipleSamurai();
+
+            InsertMultipleDifferentObjects();
         }
+
+        private static void InsertMultipleDifferentObjects()
+        {
+            var samurai = new Samurai { Name = "Dhruv" };
+
+            var battle = new Battle
+            {
+                Name = "Battle of Nagashino",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddDays(1)
+            };
+
+            using (var context = new SamuraiContext())
+            {
+                //Different data types can be added at once.
+                //Even we can directly add to context object.
+                context.AddRange(samurai, battle);
+                context.SaveChanges();
+            }
+        }
+
+        private static void InsertSamurai()
+        {  
+            var samurai = new Samurai { Name = "Julie" };
+            var samu1 = new Samurai { Name = "Dardar" };
+            using (var context = new SamuraiContext())
+            {
+                //context.Samurais.Add(samurai);
+                context.Samurais.Add(samurai);
+                context.SaveChanges();
+            }
+        }
+        private static void InsertMultipleSamurai()
+        {
+            var samurai = new Samurai { Name = "Julie" };
+            var samu1 = new Samurai { Name = "Dardar" };
+            using (var context = new SamuraiContext())
+            {
+                //context.Samurais.Add(samurai);
+                context.Samurais.AddRange(samurai, samu1);
+                context.SaveChanges();
+            }
+        }
+
     }
 }
