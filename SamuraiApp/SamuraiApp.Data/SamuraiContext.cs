f6ecf5569b2638b6b9d0434e3b16707c880aaa45
vatansoni <vatansoni89@gmail.com>
2019-01-19 23:08:08 +0530

    add-migration relationships update-database

---
+++ SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SamuraiApp.Data
+{
+   public class SamuraiContext : DbContext
+    {
+        public SamuraiContext(DbContextOptions<SamuraiContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(
+            //     "Server = (localdb)\\mssqllocaldb; Database = SamuraiAppData; Trusted_Connection = True; ");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>()
+                .HasKey(s => new { s.SamuraiId, s.BattleId });
+        }
+    }
+}
---
+++ SamuraiApp/SamuraiApp.Domain/Battle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamuraiApp.Domain
+{
+    public class Battle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+        //public List<Samurai> Samurais { get; set; }
+
+        public List<SamuraiBattle> SamuraiBattles { get; set; }
+        
+    }
+}
